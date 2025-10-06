ba85d8a9d3c2a66773b8147ececb7bac3a3a8bb3
shiny-jenita <shinyjenita@entrans.io>
2025-10-06 14:56:16 +0530

    Update server.py

--- backfolder/server.py
+++ backfolder/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify # pyright: ignore[reportMissingImports]
 
+from flask_cors import CORS  # optional if frontend is served from another origin
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/submit', methods=['POST'])
 def submit():
